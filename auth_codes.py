77a4a1746f21c52195fb1bed8cc3d2bb9f18f9ef
ldreith <ldreith@oberlin.edu>
2020-01-04 10:41:33 -0700

    changing files

---
+++ auth_codes.py
@@ -0,0 +1,7 @@
+
+def main():
+    # parse .txt file => .csv file
+    # upload .csv file to drive & convert to google sheets document
+    # create form & data spreadsheet & add code(?) & trigger(?)
+
+main()
