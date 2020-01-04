b02e439e7f3e99f2a03e92bf20d085532e29dec7
ldreith <ldreith@oberlin.edu>
2020-01-04 10:41:09 -0700

    Transforming code parsing into a class so that it can be imported

--- auth_code_parsing.py
+++ auth_code_parsing.py
@@ -1,46 +1,45 @@
 import re
 import csv
-import sys
 
-def process_line(x):
-    return ",".join(x.split()) #removes all spaces and replaces with single comma
+class CodeParser:
 
-rx_dict = {
-    'Course': re.compile(r'^.*EXCO,(?P<course>\d{3}[A-Z]?),.*$'),
-    'Code': re.compile(r'^\(unassigned\),(?P<code>\w{6}),.*$'),
-}
+    def __init__(self,filename):
+        self.filename = filename
+        self.csv = "formatted_data.csv"
 
-def _parse_line(line):
-    for key, rx in rx_dict.items():
-        match = rx.search(line)
-        if match:
-            return key, match
-    return None, None
+    rx_dict = {
+        'Course': re.compile(r'^.*EXCO,(?P<course>\d{3}[A-Z]?),.*$'),
+        'Code': re.compile(r'^\(unassigned\),(?P<code>\w{6}),.*$'),
+    }
 
-def parse_filecontents(file_contents):
-    data = {}
-    course = 0;
-    for line in file_contents:
-        key, match = _parse_line(line)
-        if key == 'Course':
-            course = match.group('course')
-            data[course] = []
-        if key == 'Code':
-            code = match.group('code')
-            data[course].append(code)
-    return data
+    def _parse_line(self,line):
+        for key, rx in self.rx_dict.items():
+            match = rx.search(line)
+            if match:
+                return key, match
+        return None, None
 
-def print_dict(data):
-    for key in data:
-        print("EXCO ", key, "\n", data[key], "\n")
+    def parse_filecontents(self,file_contents):
+        data = {}
+        course = 0;
+        for line in file_contents:
+            key, match = self._parse_line(line)
+            if key == 'Course':
+                course = match.group('course')
+                data[course] = []
+            if key == 'Code':
+                code = match.group('code')
+                data[course].append(code)
+        return data
 
-def main():
-    with open(str(sys.argv[1])) as file:
-        file_contents = list(map(process_line, file.read().splitlines())) # process each line
-        data = parse_filecontents(file_contents)
-        with open('formatted_data.csv', 'w') as f:
-            w = csv.writer(f)
-            for key in data.keys():
-                w.writerow([key] + data[key])
+    def parse(self):
+        with open(self.filename) as file:
+            file_contents = map(lambda x : ",".join(x.split()), file.read().splitlines()) # process each line
+            data = self.parse_filecontents(file_contents)
+            with open(self.csv, 'w') as f:
+                w = csv.writer(f)
+                for key in data.keys():
+                    w.writerow([key] + data[key])
 
-main()
+# x = CodeParser("codes.txt")
+# x.parse()
